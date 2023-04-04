faf2bba7fa799ef594a1f79f8dc881e8fb682dc4
Dylan Peltier <peltdyla@gmail.com>
2023-04-04 06:07:12 -0400

    first demo

---
+++ script.js
@@ -0,0 +1,79 @@
+const spot1 = document.getElementById("spot1")
+spot1.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot1.classList.contains("taken")) {
+        spot1.classList.remove("taken")
+    } else {
+        spot1.classList.add("taken")
+    }
+})
+
+const spot2 = document.getElementById("spot2")
+spot2.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot2.classList.contains("taken")) {
+        spot2.classList.remove("taken")
+    } else {
+        spot2.classList.add("taken")
+    }
+})
+
+const spot3 = document.getElementById("spot3")
+spot3.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot3.classList.contains("taken")) {
+        spot3.classList.remove("taken")
+    } else {
+        spot3.classList.add("taken")
+    }
+})
+
+const spot4 = document.getElementById("spot4")
+spot4.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot4.classList.contains("taken")) {
+        spot4.classList.remove("taken")
+    } else {
+        spot4.classList.add("taken")
+    }
+})
+
+const spot5 = document.getElementById("spotb1")
+spot5.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot5.classList.contains("taken")) {
+        spot5.classList.remove("taken")
+    } else {
+        spot5.classList.add("taken")
+    }
+})
+
+const spot6 = document.getElementById("spotb2")
+spot6.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot6.classList.contains("taken")) {
+        spot6.classList.remove("taken")
+    } else {
+        spot6.classList.add("taken")
+    }
+})
+
+const spot7 = document.getElementById("spotb3")
+spot7.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot7.classList.contains("taken")) {
+        spot7.classList.remove("taken")
+    } else {
+        spot7.classList.add("taken")
+    }
+})
+
+const spot8 = document.getElementById("spotb4")
+spot8.addEventListener("click", function(event) {
+    console.log("clicked")
+    if (spot8.classList.contains("taken")) {
+        spot8.classList.remove("taken")
+    } else {
+        spot8.classList.add("taken")
+    }
+})
